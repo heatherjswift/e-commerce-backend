4ff03fcf5193d6270c9f6b5164704057b9e95ff7
Hede <Heatherj.swift@gmail.com>
2021-05-28 18:23:31 -0400

    add api routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -29,18 +29,48 @@ router.get('/', (req, res) => {
         }
       ]
   }).then(dbProduct => {
-    res.json(dbProduct);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
+      res.json(dbProduct);
+  }).catch(err => {
+      console.log(err);
+      res.status(500).json(err);
   })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      //is this enough here?
+      'category_id'
+    ],
+     // be sure to include its associated Category and Tag data
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  }).then(dbProduct => {
+      if (!dbProduct) {
+        res.status(404).json({ message: 'No product found with this id'});
+        return;
+      }
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
@@ -112,13 +142,14 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(400).json(err);
     });
 });
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  
 });
 
 module.exports = router;
